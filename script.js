d26a291495741188f8cb98468541854ae198b498
fiyifoluwa <fiyifoluwa@outlook.com>
2021-03-23 11:41:40 +0100

    added some others

---
+++ script.js
@@ -0,0 +1,29 @@
+// Smooth scrolling
+$("nav a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65,
+      },
+      1200
+    );
+  }
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
